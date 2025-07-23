69b9a4fc15f0d3e341d1007b36f3fcf28557267d
ansoumanana <ansoumancisse@gmail.com>
2025-07-23 15:31:37 +0200

    GDS

--- neo4j-procedure-template-2025.x/src/main/java/example/EntityResultExample.java
+++ neo4j-procedure-template-2025.x/src/main/java/example/EntityResultExample.java
@@ -23,7 +23,6 @@ public class EntityResultExample {
 	public record EntityContainer(Node node) {
 	}
 
-	// call example.allnodes() yield node return node
 	@Procedure(name = "example.allnodes", mode = Mode.READ)
 	public Stream<EntityContainer> allnodes() {
 		ResourceIterator<Node> nodes = tx.execute("MATCH (n) RETURN n").columnAs("n");
--- neo4j-procedure-template-2025.x/src/main/java/example/Join.java
+++ neo4j-procedure-template-2025.x/src/main/java/example/Join.java
@@ -6,12 +6,13 @@ import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
 
-
+/**
+ * This is an example how you can create a simple user-defined function for Neo4j.
+ */
 public class Join {
 
     @UserFunction
     @Description("example.join(['s1','s2',...], delimiter) - join the given strings with the given delimiter.")
-    //"s1,s2"
     public String join(
             @Name("strings") List<String> strings,
             @Name(value = "delimiter", defaultValue = ",") String delimiter) {
